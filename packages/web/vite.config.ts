6b3600659324e3e273b98857455c2710fd02a38f
Diovani Coelli <59318991+Diovani-C@users.noreply.github.com>
2026-01-20 18:51:29 -0300

    first commit

---
+++ packages/web/vite.config.ts
@@ -0,0 +1,16 @@
+import { defineConfig } from 'vite'
+import { devtools } from '@tanstack/devtools-vite'
+import {  tanstackRouter } from '@tanstack/router-plugin/vite'
+import solidPlugin from 'vite-plugin-solid'
+import tailwindcss from '@tailwindcss/vite'
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  plugins: [
+    devtools(),
+
+    tanstackRouter({ target: 'solid', autoCodeSplitting: true }),
+    solidPlugin(),
+    tailwindcss(),
+  ],
+})
